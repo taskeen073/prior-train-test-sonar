a45b3f4da748bddc7da2e24c1cf8231610a65221
taskeen073 <taskeen073@gmail.com>
2024-10-09 13:32:01 +0700

    first commit

---
+++ src/main/java/th/co/prior/priortraintestsonar/PriorTrainTestSonarApplication.java
@@ -0,0 +1,13 @@
+package th.co.prior.priortraintestsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorTrainTestSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriorTrainTestSonarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/th/co/prior/priortraintestsonar/PriorTrainTestSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.prior.priortraintestsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorTrainTestSonarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
